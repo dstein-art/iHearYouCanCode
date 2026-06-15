a3f502539a28291985daa3ac06eb11971b7d6a8d
dstein-art <steinster+github@gmail.com>
2026-06-15 17:01:51 -0400

    Fine tuned getting-started. Started oscillators page

--- js/live-editor.js
+++ js/live-editor.js
@@ -1,8 +1,21 @@
 (function () {
+  const DEFAULT_W = 600;
+  const DEFAULT_H = 320;
+
+  function canvasSize(row, fr) {
+    const w = +(row.dataset.canvasW || fr.dataset.canvasW) || DEFAULT_W;
+    const h = +(row.dataset.canvasH || fr.dataset.canvasH) || DEFAULT_H;
+    return { w, h };
+  }
+
   document.querySelectorAll('.example-row').forEach((row) => {
     const ta = row.querySelector('textarea.live-code');
     const fr = row.querySelector('iframe.example-frame');
     if (!ta || !fr) return;
+
+    const { w, h } = canvasSize(row, fr);
+    row.style.setProperty('--canvas-w', w);
+    row.style.setProperty('--canvas-h', h);
 
     function resize() {
       ta.style.height = 'auto';
@@ -12,7 +25,7 @@
 
     function send() {
       if (fr.contentWindow) {
-        fr.contentWindow.postMessage({ type: 'run', code: ta.value }, '*');
+        fr.contentWindow.postMessage({ type: 'run', code: ta.value, width: w, height: h }, '*');
       }
     }
 
